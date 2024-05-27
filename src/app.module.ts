b3e9f301b8a5171c4a30a5b531495bddf6a85788
Rosalito gabriel <rosalitogabriel@solaireresort.com>
2024-05-27 16:20:52 +0800

    Dual Controller Issue

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SocketModule } from './socket/socket.module';
-import { SocketController } from './socket/socket.controller';
 
 @Module({
   imports: [SocketModule],
-  controllers: [SocketController],
 })
 export class AppModule {}
